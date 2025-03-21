a7bbfd72cbb0cfccac8bda0773529222d4f7eb04
Alan Tao <atao31@gatech.edu>
2025-03-21 19:51:02 -0400

    Most recent pop up stuff

---
+++ extension/ui/popup.js
@@ -0,0 +1,6 @@
+const dyslexiaOptionButtons = document.getElementsByClassName("dyslexia-option");
+let selectedDyslexiaButton = "dyslexia-none";
+
+for (let dyslexiaOptionButton of dyslexiaOptionButtons) {
+    console.log(dyslexiaOptionButton);
+}
