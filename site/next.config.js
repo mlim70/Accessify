b48498812ac6fec86c010b3d17685dd64e9e81ac
Matthew Lim <kmmatthew8@gmail.com>
2025-03-27 17:15:26 -0400

    Reverted to @, changed config to use @ in AWS

--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
