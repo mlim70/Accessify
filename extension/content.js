864d1e35ab35c372f952add22274f16ea104e683
Chenry <henrychen0214@gmail.com>
2025-03-27 16:21:28 -0400

    b

--- extension/content.js
+++ extension/content.js
@@ -642,3 +642,27 @@ async function sendToClaude(prompt) {
     throw error;
   }
 }
+
+
+chrome.storage.sync.get('preferredFont', (data) => {
+  const preferredFont = data.preferredFont || 'Comic+Sans+MS';
+
+  // Create a link element to load the Google Web Font
+  const link = document.createElement('link');
+  link.href = `https://fonts.googleapis.com/css2?family=${preferredFont}&display=swap`;
+  link.rel = 'stylesheet';
+
+  // Append the link element to the head of the document
+  document.head.appendChild(link);
+
+  // Create a style element to apply the font to all elements
+  const style = document.createElement('style');
+  style.innerHTML = `
+    * {
+      font-family: '${preferredFont.replace(/\+/g, ' ')}', sans-serif !important;
+    }
+  `;
+
+  // Append the style element to the head of the document
+  document.head.appendChild(style);
+});
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -652,3 +652,45 @@ function resetAll() {
         console.log('All preferences reset');
     });
 }
+
+
+$(document).ready(function() {
+    const fonts = [
+      { name: 'Arial', comment: '' },
+      { name: 'Comic Sans MS', comment: '' },
+      { name: 'Courier New', comment: '' },
+      { name: 'Georgia', comment: '' },
+      { name: 'Impact', comment: '' },
+      { name: 'Times New Roman', comment: '' },
+      { name: 'Trebuchet MS', comment: '' },
+      { name: 'Verdana', comment: '' },
+      { name: 'Open Sans', comment: '' },
+      { name: 'Roboto', comment: '' },
+      { name: 'Lato', comment: '' },
+      { name: 'Montserrat', comment: '' },
+      { name: 'Oswald', comment: '' },
+      { name: 'Raleway', comment: '' },
+      { name: 'PT Sans', comment: '' },
+      { name: 'Merriweather', comment: '' },
+      { name: 'Nunito', comment: '' }
+    ];
+  
+    fonts.forEach(font => {
+      $('#fontSelect').append(new Option(font.name + font.comment, font.name.replace(/\s+/g, '+')));
+    });
+  
+    $('#fontSelect').select2({
+      placeholder: 'Select a font',
+      allowClear: true
+    });
+  
+    $('#applyFont').click(() => {
+      const selectedFont = $('#fontSelect').val();
+      chrome.storage.sync.set({ preferredFont: selectedFont }, () => {
+        console.log('Font saved:', selectedFont);
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          chrome.tabs.reload(tabs[0].id);
+        });
+      });
+    });
+  });
